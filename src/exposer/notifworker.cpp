255864bc8fc137c42777cebfd5961618a2f1607a
Maira Carvalho <m.brandao.carvalho@tue.nl>
2015-11-30 14:14:23 +0100

    Adding myalleg.h to new classes to compile in Windows

--- src/exposer/notifworker.cpp
+++ src/exposer/notifworker.cpp
@@ -4,6 +4,7 @@
  *  Created on: 28 Nov 2015
  *      Author: maira
  */
+#include "../other/myalleg.h" //needs to come first to include the windows.h header in the beginning
 
 #include <Poco/NotificationQueue.h>
 #include <Poco/Notification.h>
